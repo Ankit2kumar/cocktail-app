2924f76d3b933461be03f0744cd4bdc835a0269f
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2024-01-28 13:42:13 +0100

    Adds fixes...

--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,9 @@
 /** @format */
 
-import React, { useState, useContext, useEffect } from 'react';
-import { useCallback } from 'react';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
